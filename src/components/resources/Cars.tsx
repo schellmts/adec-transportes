63adf3df97502519d8d16a1633c670624c4159ea
Matheus <desenvolvedor02@insticorp.com.br>
2025-08-30 15:24:05 -0300

    add frota

---
+++ src/components/resources/Cars.tsx
@@ -0,0 +1,26 @@
+import { cars } from '@/utils/cars'
+import React from 'react'
+import CarCard from '../elements/CarCard';
+
+export default function Cars() {
+
+  const data = cars;
+
+  return (
+    <div className='w-full text-dark'>
+      <div className='w-1/2'>
+        <h2 className='text-5xl font-semibold pb-5'>Nossa Frota</h2>
+        <p className='pb-5'>
+          Nossa frota é composta por veículos conhecidos por sua confiabilidade, conforto e eficiência. Com uma frota uniforme, garantimos um padrão de qualidade e segurança para nossos clientes.
+        </p>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 w-full md:w-2/3 mx-auto gap-10 mt-10'>
+        {data.map((item, index) => (
+          <div key={index}>
+            <CarCard name={item.name} background={item.background} tags={item.tags} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/components/resources/Choose.tsx
+++ src/components/resources/Choose.tsx
@@ -1,21 +1,20 @@
 import { chooseElements } from '@/utils/chooseCards';
 import React from 'react';
+import { FaCalendarAlt, FaWhatsapp } from 'react-icons/fa';
 
 const data = chooseElements
 
 export default function WhyChooseUs() {
   return (
-    <div className="text-dark">
-
+    <div>
       <div className="text-left mb-8">
-        <h2 className='text-5xl font-semibold pb-5'>Por Que Escolher a Adec?</h2>
-
+        <h2 className='text-5xl font-semibold pb-5 text-dark'>Por Que Escolher a Adec?</h2>
       </div>
 
       <div className="bg-dark rounded-xl p-8 md:p-12">
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {chooseElements.map((item) => {
+          {data.map((item) => {
             const Icon = item.icon;
 
             return (
@@ -32,6 +31,26 @@ export default function WhyChooseUs() {
               </div>
             );
           })}
+        </div>
+      </div>
+
+      <div className="bg-dark rounded-3xl gap-6 w-2/3 mx-auto mt-20 p-8 md:p-12 flex flex-col items-center justify-center">
+        <h2 className='text-light text-center text-3xl font-semibold'>Somos referência em conforto, pontualidade e excelência no atendimento, proporcionando uma experiência única e exclusiva.</h2>
+        <div className='flex items-center gap-4'>
+          <a
+            href="#"
+            className='flex items-center gap-2 text-[#f2f2f2] bg-[#59A25C] px-6 py-3 rounded-lg hover:bg-green-600 transition-colors duration-300'
+          >
+            <FaWhatsapp size={20} />
+            <span className='font-semibold text-[17px]'>Contato via Whatsapp</span>
+          </a>
+          <a
+            href="#contact"
+            className='flex items-center gap-2 bg-light text-dark px-6 py-3 rounded-lg hover:bg-gray-300 transition-colors duration-300'
+          >
+            <FaCalendarAlt />
+            <span className='font-semibold text-[17px]'>Solicitar Orçamento</span>
+          </a>
         </div>
       </div>
     </div>
